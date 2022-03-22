92c14243a49ff52a49560f04c2800ae179674f70
GibbyGibs <86080218+GibbyGibs@users.noreply.github.com>
2022-03-22 15:22:11 -0400

    Created Test Libaray

---
+++ General_Testing/Random Testing/src/TestingScanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TestingScanner {
+    public static void main(String[] args) {
+        System.out.println("");
+
+        int wage;
+
+        Scanner scnr = new Scanner(System.in);
+        wage = scnr.nextInt();
+
+        scnr.close(); // This stops scanning for input from line 9
+
+        System.out.print("The salary is ");
+        System.out.println(wage * 40 * 52);
+    }
+}
