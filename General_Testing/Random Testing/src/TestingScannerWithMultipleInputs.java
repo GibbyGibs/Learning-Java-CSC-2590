5c15a6bbf9a01d26172491cebd582a5c823f49af
GibbyGibs <86080218+GibbyGibs@users.noreply.github.com>
2022-03-22 21:14:02 -0400

    little changes

--- General_Testing/Random Testing/src/TestingScannerWithMultipleInputs.java
+++ General_Testing/Random Testing/src/TestingScannerWithMultipleInputs.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
-
 public class TestingScannerWithMultipleInputs {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         int birthMonth;
         int birthYear;
-        
+
         birthMonth = scnr.nextInt();
         birthYear = scnr.nextInt();
 
         scnr.close();
-        
-        System.out.println(birthMonth+"/"+birthYear);
+
+        System.out.println(birthMonth + "/" + birthYear);
     }
-    
+
 }
