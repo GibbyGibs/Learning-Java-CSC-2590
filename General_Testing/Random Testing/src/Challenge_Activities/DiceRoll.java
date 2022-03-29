3a0b6b870d413741529759fef25ac7c4c5e727ef
GibbyGibs <86080218+GibbyGibs@users.noreply.github.com>
2022-03-29 16:48:02 -0400

    Saving work

--- General_Testing/Random Testing/src/Challenge_Activities/DiceRoll.java
+++ General_Testing/Random Testing/src/Challenge_Activities/DiceRoll.java
@@ -4,18 +4,16 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class DiceRoll {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
 
-      Scanner scnr = new Scanner(System.in);
-      Random randGen = new Random();
-      int seedVal;
+        Scanner scnr = new Scanner(System.in);
+        Random randGen = new Random();
+        int seedVal;
 
-      seedVal = scnr.nextInt();
-      randGen.setSeed(seedVal);
+        seedVal = scnr.nextInt();
+        randGen.setSeed(seedVal);
 
-      
-      System.out.println(randGen.nextInt(15));
-    
+        System.out.println(randGen.nextInt(15));
 
-   }
+    }
 }
--- General_Testing/Random Testing/src/Challenge_Activities/OutputCombinations.java
+++ General_Testing/Random Testing/src/Challenge_Activities/OutputCombinations.java
@@ -13,17 +13,20 @@ public class OutputCombinations {
         b = scnr.next().charAt(0);
         c = scnr.next().charAt(0);
 
-        System.out.print(""+ a+b+c + " " + a+c+b + " " + b+a+c + " " + b+c+a + " " + c+a+b + " " + c+b+a);
+        System.out.print("" + a + b + c + " " + a + c + b + " " + b + a + c + " " + b + c + a + " " + c + a + b + " "
+                + c + b + a);
 
         System.out.println("");
     }
 }
 
-// Output all combinations of character variables a, b, and c, in the order shown below. If a = 'x', b = 'y', and c = 'z', then the output is:
+// Output all combinations of character variables a, b, and c, in the order
+// shown below. If a = 'x', b = 'y', and c = 'z', then the output is:
 
 // xyz xzy yxz yzx zxy zyx
 
-// Note: If outputting multiple character variables with one statement, the argument for System.out.print() should start with "" +
+// Note: If outputting multiple character variables with one statement, the
+// argument for System.out.print() should start with "" +
 
-// Your code will be tested in three different programs, with a, b, c assigned with 'x', 'y', 'z', then with '#', '$', '%', then with '1', '2', '3'.
-
+// Your code will be tested in three different programs, with a, b, c assigned
+// with 'x', 'y', 'z', then with '#', '$', '%', then with '1', '2', '3'.
