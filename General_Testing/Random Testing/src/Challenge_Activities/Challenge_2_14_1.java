882b67d8253c87cea2de481f6a2e0dc917b1c70d
GibbyGibs <86080218+GibbyGibs@users.noreply.github.com>
2022-03-29 16:31:49 -0400

    Doing HomeWork

---
+++ General_Testing/Random Testing/src/Challenge_Activities/Challenge_2_14_1.java
@@ -0,0 +1,19 @@
+package Challenge_Activities;
+
+import java.util.Scanner;
+
+public class Challenge_2_14_1 {
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+        char letterToQuit;
+        int numPresses;
+
+        letterToQuit = scnr.next().charAt(0);
+        numPresses = scnr.nextInt();
+
+        if (numPresses == 2 && letterToQuit == 'q')
+            System.out.println("You have sucessfully quit");
+
+    }
+}
---
+++ General_Testing/Random Testing/src/Challenge_Activities/DiceRoll.java
@@ -0,0 +1,21 @@
+package Challenge_Activities;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class DiceRoll {
+    public static void main(String[] args) { 
+
+      Scanner scnr = new Scanner(System.in);
+      Random randGen = new Random();
+      int seedVal;
+
+      seedVal = scnr.nextInt();
+      randGen.setSeed(seedVal);
+
+      
+      System.out.println(randGen.nextInt(15));
+    
+
+   }
+}
