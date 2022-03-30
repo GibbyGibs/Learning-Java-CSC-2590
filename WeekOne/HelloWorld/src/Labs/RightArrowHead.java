50cca7bcb8a1ef4ffa4afba5e82f54779f206591
GibbyGibs <Codmister96@gmail.com>
2022-03-30 15:50:39 -0400

    Working on Labs for module 2

---
+++ WeekOne/HelloWorld/src/Labs/RightArrowHead.java
@@ -0,0 +1,29 @@
+package Labs;
+
+import java.util.Scanner;
+
+public class RightArrowHead {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int baseChar;
+        int headChar;
+
+        /* Type your code here. */
+
+
+        /*
+            ! Should look like this.
+
+             1
+             11
+        00000111
+        000001111
+        00000111
+             11
+             1
+
+
+
+        */
+    }
+}
