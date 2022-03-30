0e0c21c5634f5937b9edc29371ef5d46fe4c2e88
GibbyGibs <86080218+GibbyGibs@users.noreply.github.com>
2022-03-29 21:57:56 -0400

    Working on more labs

---
+++ WeekOne/HelloWorld/src/Labs/SimpleStatistics.java
@@ -0,0 +1,50 @@
+package Labs;
+
+import java.util.Scanner;
+
+public class SimpleStatistics {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int num1 = 10000;
+        int num2 = 20000;
+        int num3 = 30000;
+        int num4 = 50000;
+
+
+        /*
+         ! This NEEDS TO BE WORKED ON OR MENTION IT IN CLASS
+
+        */
+
+        // int temp1;
+        // int temp2;
+        
+        // Gets the Numbers to be calculated from the user
+        // num1 = scnr.nextLong();
+        // num2 = scnr.nextLong();
+        // num3 = scnr.nextLong();
+        // num4 = scnr.nextLong();
+
+        //multiply all 4 and you get total
+        // temp1 = num1 * num2 * num3 * num4;
+
+        // adds all for and then divides by 4
+        // temp2 = (num1 + num2 + num3 + num4) / 4;
+
+        // System.out.print(temp1);
+
+        // System.out.println(temp1 + " " + temp2); 
+        
+        // double temp3 = Double.valueOf(temp1);
+        // double temp4 = Double.valueOf(temp2 + 0.750);
+
+        // System.out.printf("%.3f", temp3);
+        // System.out.print(" ");
+        // System.out.printf("%.3f", temp4);
+        // System.out.println("");
+
+
+
+    }
+
+}
---
+++ WeekOne/HelloWorld/src/Labs/UsingMathMethods.java
@@ -0,0 +1,28 @@
+package Labs;
+
+import java.util.Scanner;
+
+public class UsingMathMethods {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double x;
+        double y;
+        double z;
+        double temp1;
+        double temp2;
+        double temp3;
+        double temp4;
+
+        x = scnr.nextDouble();
+        y = scnr.nextDouble();
+        z = scnr.nextDouble();
+
+        temp1 = Math.pow(x, z);
+        temp2 = Math.pow(x, Math.pow(y, z));
+        temp3 = Math.abs(y);
+        temp4 = Math.sqrt(Math.pow(x * y, z));
+
+        System.out.println(temp1 + " " + temp2 + " " + temp3 + " " + temp4);
+
+    }
+}
